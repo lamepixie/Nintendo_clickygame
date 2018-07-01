1b68fe17579120f5557844da216cfc0db4bf7a9b
Joellen Terranova <joellen.leigh@gmail.com>
2018-06-30 22:39:40 -0400

    small front end adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 const Header = () => (
-  <div className="nes-container">
-    <div className="cartridge">
-      <p className="text-left info">Click on the Characters!</p>
-      <p className="text-left sub">Click on a character to earn points, don't click the same character twice!</p>
-      <div className="nub"></div>
-    </div>
+  <div className="game">
+    <div className="nes-container">
+      <div className="cartridge">
+        <p className="text-left info">Click on the Characters!</p>
+        <p className="text-left sub">Click on a character to earn points, don't click the same character twice!</p>
+        <div className="nub"></div>
+      </div>
 
-    <div className="black"></div>
+      <div className="black"></div>
+    </div>
   </div>
 
 )
